e076b211b6b426dac3d56be82802c60c2fdf518d
Steven Lung <1030steven@gmail.com>
2022-09-29 16:33:32 +0800

    Add incorrect python file for git practice

---
+++ wrong.py
@@ -0,0 +1,4 @@
+# Add two inputs
+a = eval(input('please enter teh first nubmer: '))
+b = eval(input('please enter the second number: '))
+print("The first number adding the second number is: ",a-b)
