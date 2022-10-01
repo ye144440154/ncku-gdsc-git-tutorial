927274e5d04d264b33eb40c72d48fb1b473a0f1e
魏子翔 <andy144440154@gmail.com>
2022-10-01 16:02:02 +0800

    add the minus feature adding minus function

--- wrong.py
+++ wrong.py
@@ -1,4 +1,4 @@
 # Add two inputs
-a = eval(input('please enter the first number: '))
+a = eval(input('please enter teh first number: '))
 b = eval(input('please enter the second number: '))
-print("The first number adding the second number is: ",a+b)
+print("The first number adding the second number is: ",a-b)
