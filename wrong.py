f4a5498d56f074e271dae51495ab3c8368e2f54f
魏子翔 <andy144440154@gmail.com>
2022-10-01 15:40:05 +0800

    The perfect version without any bugs

--- wrong.py
+++ wrong.py
@@ -1,4 +1,4 @@
 # Add two inputs
-a = eval(input('please enter teh first nubmer: '))
+a = eval(input('please enter the first number: '))
 b = eval(input('please enter the second number: '))
-print("The first number adding the second number is: ",a-b)
+print("The first number adding the second number is: ",a+b)
